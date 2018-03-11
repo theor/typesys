5aad7264c788e056a430aa349586b60ad5a49d92
theor <theo.richart@gmail.com>
2018-03-11 12:27:44 -0400

    wip parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,22 @@
+#[macro_use]
+extern crate nom;
+
 pub mod errors;
 pub mod ast;
 pub mod types;
+pub mod parser;
+
 
 #[cfg(test)]
 mod tests {
+    #[test]
+    fn parse() {
+        assert_eq!(::parser::expr(b"12+6-4+3"), ::parser::IResult::Done(&b""[..], 17));
+    }
+    #[test]
+    fn parse2() {
+        assert_eq!(::parser::ptype(b"qwe"), ::parser::IResult::Done(&b""[..], "qwe"));
+    }
     #[test]
     fn it_works() {
         use super::types::check;
